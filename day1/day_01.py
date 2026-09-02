adc50ecd7870c16f48822a6473df1fc2ebece5a0
Said Rustamov <saidrustamov011@gmail.com>
2026-09-02 02:43:27 -0700

    Day 1: Introduction - data types and arithmetic operators

---
+++ day1/day_01.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+print(5 + 2)
+print(31 - 4)
+print(81 / 9)
+print(10 * 9)
+
+print(5 ** 2)
+print(13 % 3)
+print(14 // 3)
+
+print(type(5)) #int-integer
+print(type(3.4)) #float
+print(type(1 + 3j)) #complex number
+print(type("house")) #str-string
+print(type([1, 2, 3])) #list
+print(type({"name": "Said"})) #dict-dictionary
+print(type({9.4 , 10.3 , 4.5})) #Set
+print(type((9.8, 3.14, 2.7)))  #tuple
---
+++ day1_exercise.py
@@ -0,0 +1,14 @@
+print(3+4)
+print(4*4)
+print("SAID")
+print({'Car': 'BMW'})
+
+
+print(5)
+print(type(5))
+
+print(3.14)
+print(type(3.14))
+
+print([1, 2, 3])
+print(type([1, 2, 3]))
