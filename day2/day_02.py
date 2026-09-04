9ed1b94366df5a3a210ae77d1d25c00be7b389ba
Said Rustamov <saidrustamov011@gmail.com>
2026-09-04 02:41:38 -0700

    Learned main built-in functions

---
+++ day2/day_02.py
@@ -0,0 +1,28 @@
+print('Hello World!')
+
+a = len('Hello, World!')
+#len counts the number of characters including space
+print(a)
+
+b = type('Hello, World!')
+#type check the data type
+print(b)
+
+c = str(10)
+#str converts the number to string (so I cannot just write c + 2 = 12)
+print(c)
+
+#print(c + 2)
+
+z = int(10)
+#int converts to number 10 
+
+x = float(10)
+#convers the integer to decimal (from 10 to 10.0)
+print(x)
+
+input("Enter your name: ") #takes user input
+
+min(11, 22, 33, 44, 55)
+max(11, 22, 33, 44, 55)
+sum(11, 22, 33, 44, 55)
